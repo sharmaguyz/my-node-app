bf27bb1b34cbbecad90e6b3a3e2575f3291ca4dd
amit sharma <48862526+sharmaguyz@users.noreply.github.com>
2025-11-10 00:29:35 +0530

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-set -e
-APP_DIR="/home/ubuntu/my-node-app"
-sudo apt-get update -y && sudo apt-get install -y dos2unix
-find "$APP_DIR/scripts" -type f -name "*.sh" -exec dos2unix {} \;
-chmod +x "$APP_DIR"/scripts/*.sh
+
