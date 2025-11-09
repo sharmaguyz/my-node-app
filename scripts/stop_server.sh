d871a45293e74eaf1c08ae9fd029edba651e7fca
amit sharma <48862526+sharmaguyz@users.noreply.github.com>
2025-11-09 22:00:40 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Stop and remove the app from PM2 if it exists
 if pm2 describe my-node-app >/dev/null 2>&1; then
   pm2 stop my-node-app || true
   pm2 delete my-node-app || true
